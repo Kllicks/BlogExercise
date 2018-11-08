812bc27995a514af529acef19d11e13018fd95d9
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-08 12:46:51 -0500

    adds tables and creation of tables

---
+++ schema.sql
@@ -0,0 +1,29 @@
+-- AUTHORs
+    -- name
+create table authors (
+    id serial primary key,
+    name text
+);
+
+-- POSTs
+    -- title
+    -- postContent
+    -- postDate
+create table posts (
+    id serial primary key,
+    postContent text,
+    postDate date,
+    author_id integer references authors (id)
+);
+
+
+-- COMMENTs
+    -- commentContent
+    -- commentDate
+create table comments (
+    id serial primary key,
+    commentContent text,
+    commentDate date,
+    author_id integer references authors (id)
+)
+
---
+++ seed.sql
@@ -0,0 +1,26 @@
+insert into authors
+    (name)
+values
+    ('Al'),
+    ('Bob'),
+    ('Carl'),
+    ('Don'),
+    ('Fran');
+
+insert into posts
+    (postContent, postDate)
+values
+    ('some content', '2018-01-01'),
+    ('this is also content', '2017-02-10'),
+    ('placeholder content', '2018-10-20'),
+    ('summary', '2018-05-25'),
+    ('example number five', '2017-11-18');
+
+insert into comments
+    (commentContent, commentDate)
+values
+    ('like', '2018-10-10'),
+    ('dislike', '2018-02-01'),
+    ('neutral', '2018-06-20'),
+    ('happy', '2018-12-05'),
+    ('cool', '2018-09-17');
